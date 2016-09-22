c2674941d68e3352d9e5dd5812b567f2ecfa2c2a
root <root@localhost.localdomain>
2016-09-22 00:36:48 -0400

    Try Again

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -7,9 +7,9 @@ bootstrapDir="/usr/share/nginx-bootstrap"
 if [ -e "$bootstrapDir/new-flag" ]
 then
   echo "Extracting files"
-  gosu tar xvf $bootstrapDir/etc-nginx.tar.gz -C /etc/nginx
-  gosu tar xvf $bootstrapDir/srv-http.tar.gz -C /srv/http
-  gosu rm $bootstrapDir/new-flag
+  gosu root tar xvf $bootstrapDir/etc-nginx.tar.gz -C /etc/nginx
+  gosu root tar xvf $bootstrapDir/srv-http.tar.gz -C /srv/http
+  gosu root rm $bootstrapDir/new-flag
 fi
 
-exec gosu nginx -g daemon-off;
+exec gosu www-data nginx -g daemon-off;
