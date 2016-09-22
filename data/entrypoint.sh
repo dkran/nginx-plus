e2ccea1727cd2cd24bbf9c8d50457e0d5984e7c7
root <root@localhost.localdomain>
2016-09-22 01:17:16 -0400

    Setting more permissions

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -7,6 +7,8 @@ bootstrapDir="/usr/share/nginx-bootstrap"
 if [ -e "$bootstrapDir/new-flag" ]
 then
   echo "Extracting files"
+  gosu root chown -R 1010:1010 /etc/nginx /var/log/nginx /srv/http /usr/share/nginx-bootstrap
+  gosu root chmod -R 775 /etc/nginx /var/log/nginx /srv/http /usr/share/nginx-bootstrap
   gosu nginx tar xvf $bootstrapDir/etc-nginx.tar.gz -C /etc/nginx
   gosu nginx tar xvf $bootstrapDir/srv-http.tar.gz -C /srv/http
   gosu nginx rm $bootstrapDir/new-flag
